0f7a9b108d181231a042a18e9cdfeaaacd279b5d
Paradoxon <two4god@gmail.com>
2017-02-17 23:15:38 +0100

    added header for a Panel and a Window

---
+++ SettingsPanel.h
@@ -0,0 +1,10 @@
+#ifndef SETTINGS_PANEL_H
+#define SETTINGS_PANEL_H
+
+#include "SettingsHandler.h"
+#include "Settings.h"
+
+
+#endif	// SETTINGS_PANEL_H
+
+
---
+++ SettingsWindows.h
@@ -0,0 +1,10 @@
+#ifndef SETTINGS_WINDOW_H
+#define SETTINGS_WINDOW_H
+
+#include "SettingsHandler.h"
+#include "Settings.h"
+
+
+#endif	// SETTINGS_WINDOW_H
+
+
